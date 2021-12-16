b88b8dafdde97f5fa414dbd8954678e844023e31
mahirkhan0828 <59534578+mahirkhan0828@users.noreply.github.com>
2021-12-16 12:46:13 -0600

    Update index.js

--- AnimaPackage-React-Mobile/index.js
+++ AnimaPackage-React-Mobile/index.js
@@ -1,7 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./globals.css"
-import "./styleguide.css"
-import App from "./App";
+import "./globals.css";
+import "./styleguide.css";
+import MobileApp from "./MobileApp";
+import DesktopApp from "../AnimaPackage-React-Desktop/DesktopApp";
 
-ReactDOM.render(<App />, document.getElementById("app"));
+// Check window size and render based on if mobile not
+function checkWindowSize(x) {
+    if (x.matches) { // If media query matches
+    //   document.body.style.backgroundColor = "yellow";
+      ReactDOM.render(<MobileApp />, document.getElementById("app"));
+    } else {
+    //   document.body.style.backgroundColor = "pink";
+      ReactDOM.render(<DesktopApp />, document.getElementById("app"));
+    }
+  }
+  
+  var x = window.matchMedia("(max-width: 600px)")
+  checkWindowSize(x);
+
